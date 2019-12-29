bb0d00e4fdad51e59853d8c2821d012eeb54987f
Marlon Algarra <algarramarlon@hotmail.com>
2019-12-29 17:40:26 -0500

    Day 2 wiht aditional func

---
+++ Day 2/opcode.m
@@ -0,0 +1,26 @@
+function c=opcode(inp,j,k)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+inp(2)=j; inp(3)=k;
+for i=1:4:length(inp)
+    if i<145
+        temp1=inp(1,i+1);
+        temp2=inp(1,i+2);
+        temp3=inp(1,i+3);
+        
+        if inp(1,i)==1
+            inp(1,temp3+1)=inp(temp1+1)+inp(1,temp2+1);
+        end
+        
+        if inp(1,i)==2
+            inp(1,temp3+1)=inp(temp1+1)*inp(1,temp2+1);
+        end
+        
+        if inp(1,i)==99
+            continue
+        end
+    end
+end
+c=inp;
+end
+
